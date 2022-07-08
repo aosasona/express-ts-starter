1317e7709d13d53f48d7289602be944c32e3af94
Ayodeji O <osasonaayodeji1@gmail.com>
2022-07-08 09:11:31 +0100

    Added tests

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,32 @@
 import express, { Express, Request, Response } from "express";
-import { connect, disconnect } from "./services/db";
 import helmet from "helmet";
 import cors from "cors";
+import hpp from "hpp";
+import toobusy_js from "toobusy-js";
 
 const app: Express = express();
 
 //APP MIDDLE-WARES
 // Middleware
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.disable("x-powered-by");
-
 if (process.env.NODE_ENV === "production") {
   app.use(helmet());
 }
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(hpp());
+app.disable("x-powered-by");
 
 //APP ROUTES - IMPORT
 import { default as routes } from "./routes";
+
+// TOOBUSY
+app.use((req: Request, res: Response, next: Function) => {
+  if (toobusy_js()) {
+    return res.status(429).send("Too busy!");
+  }
+  next();
+});
 
 app.use("/test", routes);
 
--- src/controllers/dummy.controller.ts
+++ src/controllers/dummy.controller.ts
@@ -1,13 +1,16 @@
 import { Request, Response, RequestHandler } from "express";
 import asyncHandler from "express-async-handler";
 import Dummy from "../models/dummy.model";
+import CustomResponse from "../utils/handlers/response.handler";
 
 export const dummyController: RequestHandler = asyncHandler(
   async (req: Request, res: Response): Promise<any> => {
     try {
       const Test: any = await Dummy.find().select(["name", "-_id"]).lean();
 
-      return res.status(200).send(Test);
+      return new CustomResponse(res).success("Dummy data", { Test }, 200, {
+        type: "test",
+      });
     } catch (e: any) {
       return res.status(500).send(e.message);
     }
